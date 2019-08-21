f20f21f4216881a726bfb81a60810bbef3a6df76
ASHOK MAHARJAN <ashok997@gmail.com>
2019-08-21 23:53:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,8 @@ class App < Sinatra::Base
   end
 
   get '/set' do
-     session [:foo] = 'hello'
+    session ['foo'] = 'hello'
+    
     if session[:foo] == 'hello'
       redirect '/fetch'
     else
